b53c144f618b3ce18892b6a064d0c44f2f17810f
Oskar Szarmach <szarmach.o@gmail.com>
2023-10-03 15:03:56 +0200

    (Added) Buff - extra game ball

---
+++ Assets/Scripts/Buffs and debuffs/ExtraBall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraBall : BuffEntity
+{
+    [SerializeField] GameObject gameBallPref;
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null)
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            float newY = player.transform.position.y + 0.7f;
+            Vector3 newBallPos = new Vector3(player.transform.position.x, newY);
+
+            Instantiate(gameBallPref, newBallPos, Quaternion.identity);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
